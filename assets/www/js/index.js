5c36fe9e352c1d299ea9d2fbfbec19de78f5b6a6
skylarmt <skylarmt@users.noreply.github.com>
2014-06-29 21:04:14 -0600

    Update index.js

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -1,5 +1,8 @@
 var app = {
     initialize: function() {
+        $(document).bind('touchmove', function(e) {
+        	e.preventDefault();
+        });
         $("#link_chat").click(function(){
             $('#page').load('inc/chat.html');
             return false;
